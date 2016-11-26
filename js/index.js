9a4137612fb0877bc00587bbe5df4b314c03ae37
Brian Barrow <briancbarrow@gmail.com>
2016-11-26 14:45:19 -0700

    modify reducers, add store

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 require('babel-polyfill');
 
+import * as actions from './actions/index';
+import store from './store';
+// import * as reducers from './reducers/index';
+
+store.dispatch(actions.addRepository('joe'));
+console.log(store.getState());
