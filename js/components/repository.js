f16e4e7c1a8bd89998e619f15b98f989e1b4e026
Brian Barrow <briancbarrow@gmail.com>
2016-12-07 06:31:26 -0700

    learning async with redux-thunk, isomorphic-fetch

--- js/components/repository.js
+++ js/components/repository.js
@@ -5,27 +5,33 @@ import StarRater from './star-rater';
 import * as actions from '../actions/index';
 
 export class Repository extends React.Component {
-    constructor(props) {
-        super(props);
-        this.changeRating = this.changeRating.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.changeRating = this.changeRating.bind(this);
+  }
 
-    changeRating(rating) {
-        this.props.dispatch(
-            actions.rateRepository(this.props.repository.name, rating)
-        );
-    }
+  componentDidMount() {
+    this.props.dispatch(
+      actions.fetchDescription(this.props.repository.name)
+    );
+  }
 
-    render() {
-        return (
-            <div className="repository">
-                {this.props.repository.name}
-                &nbsp;
-                <StarRater rating={this.props.repository.rating}
-                           onChange={this.changeRating} />
-            </div>
-        );
-    }
+  changeRating(rating) {
+    this.props.dispatch(
+      actions.rateRepository(this.props.repository.name, rating)
+    );
+  }
+
+  render() {
+    return (
+      <div className="repository">
+        {this.props.repository.name} - {this.props.repository.description}
+        &nbsp;
+        <StarRater rating={this.props.repository.rating}
+                     onChange={this.changeRating} />
+      </div>
+    );
+  }
 }
 
 export default connect()(Repository);
